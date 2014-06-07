488f1390776e7ace27ae2a5718a3a051fa3ca364
skyline75489 <skyline75489@outlook.com>
2014-06-07 19:50:52 +0800

    Make it PEP8

--- hitlib2/hitlib2.py
+++ hitlib2/hitlib2.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 import argparse
 import re
 import sys
@@ -10,17 +10,18 @@ from BeautifulSoup import BeautifulSoup
 BASE_URL = "http://202.118.250.131/lib/opacAction.do?method=DoAjax&dispatch=smcx&type=smcx&method1=1&retrieveLib=1"
 BOOK_DETAIL_URL = "http://202.118.250.131/lib/opacAction.do?method=DoAjax&dispatch=searchBiblInfo&type=searchBiblInfo&book_type=589&currpage=1&id_bibl="
 
-#书目
-SM_TYPE="589"
-#期刊
-QK_TYPE="590"
-#论文
-LW_TYPE="666"
+# 书目
+SM_TYPE = "589"
+# 期刊
+QK_TYPE = "590"
+# 论文
+LW_TYPE = "666"
 
 
 def _make_sm_dict(info):
-    return Book({'title':info[0], 'author':info[1], 'publisher':info[2], 
-                 'year':info[3], 'pages':info[4], 'sn':info[5], 'id':info[7]})
+    return Book({'title': info[0], 'author': info[1], 'publisher': info[2],
+                 'year': info[3], 'pages': info[4], 'sn': info[5], 'id': info[7]})
+
 
 def _make_qk_dict(info):
     info_dict = {}
@@ -31,6 +32,7 @@ def _make_qk_dict(info):
     info_dict['id'] = info[7]
     return info_dict
 
+
 def _make_lw_dict(info):
     info_dict = {}
     info_dict['title'] = info[0]
@@ -40,14 +42,15 @@ def _make_lw_dict(info):
     info_dict['id'] = info[7]
     return info_dict
 
+
 class Book(dict):
 
     def _request_for_detail(self):
-    	try:
-    	    response = requests.get(BOOK_DETAIL_URL + str(self['id']))
-    	except requests.exceptions.RequestException as e:
+        try:
+            response = requests.get(BOOK_DETAIL_URL + str(self['id']))
+        except requests.exceptions.RequestException as e:
             print(e.message)
-            
+
         self._detail_raw = response.content
         return BeautifulSoup(self._detail_raw)
 
@@ -55,18 +58,18 @@ class Book(dict):
         self['shelf'] = []
         a = f.findAll('td')
         list_len = len(a)
-	# digest the response page we got
-        for i in range(47, list_len-4, 4):
+        # digest the response page we got
+        for i in range(47, list_len - 4, 4):
             shelf_info = {}
             shelf_info['num'] = a[i].text
-            shelf_info['pos'] = a[i+1].text
-            shelf_info['type'] = a[i+2].text
-            shelf_info['status'] = a[i+3].text
-            shelf_info['return'] = a[i+4].text
+            shelf_info['pos'] = a[i + 1].text
+            shelf_info['type'] = a[i + 2].text
+            shelf_info['status'] = a[i + 3].text
+            shelf_info['return'] = a[i + 4].text
             if not shelf_info['num'] or not shelf_info['pos']:
                 continue
             for k in shelf_info.keys():
-		# unwanted info, break out 
+                # unwanted info, break out
                 if shelf_info[k].find('nbsp') > 0:
                     return
             self['shelf'].append(shelf_info)
@@ -79,29 +82,30 @@ class Book(dict):
         return self['shelf']
 
     def show(self):
-    	print(unicode('{0[title]}\n'
-    		      '{0[author]}\n'
-    		      '{0[publisher]}\n'
-    		      '{0[year]}\n'
-    		      '{0[pages]}\n'
-    		      '{0[sn]}\n'
-    		      '{0[id]}').format(self))
+        print(unicode('{0[title]}\n'
+                      '{0[author]}\n'
+                      '{0[publisher]}\n'
+                      '{0[year]}\n'
+                      '{0[pages]}\n'
+                      '{0[sn]}\n'
+                      '{0[id]}').format(self))
 
     def shelf(self):
         for i in self._get_result():
-            print(unicode('{0[num]}\t\t{0[pos]}\t\t{0[type]}\t\t{0[status]}\t{0[return]}').format(i))
-            
+            print(unicode(
+                '{0[num]}\t\t{0[pos]}\t\t{0[type]}\t\t{0[status]}\t{0[return]}').format(i))
+
 
 class Query(object):
 
     def __init__(self, keyword, q_type="sm", page=1):
         self.keyword = keyword
         self.page = page
-        #cache the result
+        # cache the result
         self.result = None
         self.raw = None
         self.q_type = q_type.lower()
-        
+
         if q_type not in ("sm", "qk", "lw"):
             raise TypeError("Wrong type")
 
@@ -117,7 +121,7 @@ class Query(object):
             "lw": LW_TYPE
         }
 
-    def show(self, keyword = "title"):
+    def show(self, keyword="title"):
         for i in self._get_result():
             if isinstance(i, dict):
                 print(i[keyword])
@@ -132,19 +136,19 @@ class Query(object):
 
         self.result = self._parse_html(self._requests())
         return self.result
-        
+
     def _requests(self):
         try:
-            response = requests.get(BASE_URL + "&title=" + self.keyword + "&pabookType=" + 
-                       self.typeMap[self.q_type] + "&smcx_p=" + str(self.page))
+            response = requests.get(BASE_URL + "&title=" + self.keyword + "&pabookType=" +
+                                    self.typeMap[self.q_type] + "&smcx_p=" + str(self.page))
         except requests.exceptions.RequestException as e:
             print(e.message)
-            
+
         self.raw = response.content
         return BeautifulSoup(self.raw)
 
     def _parse_html(self, f):
-        return [self.typeFunctionMap[self.q_type](self._digest_info(tr)) for tr in 
+        return [self.typeFunctionMap[self.q_type](self._digest_info(tr)) for tr in
                 f.findAll(onmouseover=True)]
 
     def _digest_info(self, tr):
@@ -157,20 +161,26 @@ class Query(object):
             text = text.replace("=", "")
             text = text.replace("\x1e", " ")
             info.append(text)
-            
+
         # the id for more detail
         info.append(int(id_reg.findall(id_text)[0]))
         return info
 
+
 def get_parser():
-    parser = argparse.ArgumentParser(description='Tiny Python library servering as a set of APIs for HIT Online Library')
+    parser = argparse.ArgumentParser(
+        description='Tiny Python library servering as a set of APIs for HIT Online Library')
     parser.add_argument('query', metavar='QUERY', type=str, nargs='*',
-                        help='query the book info')    
-    parser.add_argument('-t', '--type', help='book type of query: sm (shumu), qk (qikan), lw (lunwen)', default='sm', type=str)    
-    parser.add_argument('-a','--all', help='display the full info of the result')
-    parser.add_argument('-p','--pos', help='select book info in specified position (default: 1)')
+                        help='query the book info')
+    parser.add_argument(
+        '-t', '--type', help='book type of query: sm (shumu), qk (qikan), lw (lunwen)', default='sm', type=str)
+    parser.add_argument(
+        '-a', '--all', help='display the full info of the result')
+    parser.add_argument(
+        '-p', '--pos', help='select book info in specified position (default: 1)')
 
     return parser
+
 
 def command_line_runner():
     parser = get_parser()
@@ -182,6 +192,7 @@ def command_line_runner():
 
     query(args)
     return
+
 
 def query(args):
     f = Query(' '.join(args['query']), args['type'])
